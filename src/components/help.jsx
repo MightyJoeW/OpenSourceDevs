f62d18e34ba3b2aecb5f18301910c311c1e63df8
Joe Warren <joe.warren7@protonmail.com>
2021-09-30 08:09:33 -0500

    fix: setup help page contale of contents

--- src/components/help.jsx
+++ src/components/help.jsx
@@ -1,28 +1,156 @@
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+
 const Help = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <>
-      <h2>How to Contribute</h2>
+    <div style={{ width: '70%', margin: ' auto' }}>
+      <div>
+        <Button
+          id='basic-button'
+          aria-controls='basic-menu'
+          aria-haspopup='true'
+          aria-expanded={open ? 'true' : undefined}
+          onClick={handleClick}
+        >
+          Table of Contents
+        </Button>
+        <Menu
+          id='basic-menu'
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            'aria-labelledby': 'basic-button',
+          }}
+        >
+          <MenuItem onClick={handleClose}>
+            <a href='#how-to-create-a-github-account'>
+              How to create a GitHub account
+            </a>
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <a href='#finding-an-open-source-project'>
+              Finding an open source project
+            </a>
+          </MenuItem>
+          <MenuItem onClick={handleClose}>
+            <a href='#how-to-submit-contributions'>
+              How to submit contributions
+            </a>
+          </MenuItem>
+        </Menu>
+      </div>
+      <h2 id='how-to-create-a-github-account'>
+        Section 1 - How to Create a GitHub account
+      </h2>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, unde
-        temporibus deleniti expedita sint consequuntur distinctio consequatur
-        maiores reprehenderit nostrum cupiditate recusandae quibusdam
-        praesentium vero voluptatem ullam maxime? In, nisi.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum
+        provident unde fugit distinctio nobis ipsa repudiandae maiores totam
+        numquam. Dolore excepturi atque voluptates velit dolor ad possimus
+        nesciunt quae asperiores? Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Earum ea reprehenderit velit voluptates facilis, nulla
+        et architecto dolor, aliquid corporis, incidunt blanditiis excepturi
+        voluptatem dolores quaerat culpa fuga esse qui?
       </p>
-      <img src='#' alt='Forking a GitHub project' />
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, unde
-        temporibus deleniti expedita sint consequuntur distinctio consequatur
-        maiores reprehenderit nostrum cupiditate recusandae quibusdam
-        praesentium vero voluptatem ullam maxime? In, nisi.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum
+        provident unde fugit distinctio nobis ipsa repudiandae maiores totam
+        numquam. Dolore excepturi atque voluptates velit dolor ad possimus
+        nesciunt quae asperiores? Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Earum ea reprehenderit velit voluptates facilis, nulla
+        et architecto dolor, aliquid corporis, incidunt blanditiis excepturi
+        voluptatem dolores quaerat culpa fuga esse qui?
       </p>
-      <a
-        href='https://github.com/MightyJoeW/OpenSourceAllstars'
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        Open Source Allstars Github Repo
-      </a>
-    </>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum
+        provident unde fugit distinctio nobis ipsa repudiandae maiores totam
+        numquam. Dolore excepturi atque voluptates velit dolor ad possimus
+        nesciunt quae asperiores? Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Earum ea reprehenderit velit voluptates facilis, nulla
+        et architecto dolor, aliquid corporis, incidunt blanditiis excepturi
+        voluptatem dolores quaerat culpa fuga esse qui?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum
+        provident unde fugit distinctio nobis ipsa repudiandae maiores totam
+        numquam. Dolore excepturi atque voluptates velit dolor ad possimus
+        nesciunt quae asperiores? Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Earum ea reprehenderit velit voluptates facilis, nulla
+        et architecto dolor, aliquid corporis, incidunt blanditiis excepturi
+        voluptatem dolores quaerat culpa fuga esse qui?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum
+        provident unde fugit distinctio nobis ipsa repudiandae maiores totam
+        numquam. Dolore excepturi atque voluptates velit dolor ad possimus
+        nesciunt quae asperiores? Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Earum ea reprehenderit velit voluptates facilis, nulla
+        et architecto dolor, aliquid corporis, incidunt blanditiis excepturi
+        voluptatem dolores quaerat culpa fuga esse qui?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum
+        provident unde fugit distinctio nobis ipsa repudiandae maiores totam
+        numquam. Dolore excepturi atque voluptates velit dolor ad possimus
+        nesciunt quae asperiores? Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Earum ea reprehenderit velit voluptates facilis, nulla
+        et architecto dolor, aliquid corporis, incidunt blanditiis excepturi
+        voluptatem dolores quaerat culpa fuga esse qui?
+      </p>
+      <h2 id='finding-an-open-source-project'>
+        Section 2 - How to Create a GitHub account
+      </h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum
+        provident unde fugit distinctio nobis ipsa repudiandae maiores totam
+        numquam. Dolore excepturi atque voluptates velit dolor ad possimus
+        nesciunt quae asperiores?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum
+        provident unde fugit distinctio nobis ipsa repudiandae maiores totam
+        numquam. Dolore excepturi atque voluptates velit dolor ad possimus
+        nesciunt quae asperiores?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum
+        provident unde fugit distinctio nobis ipsa repudiandae maiores totam
+        numquam. Dolore excepturi atque voluptates velit dolor ad possimus
+        nesciunt quae asperiores?
+      </p>
+      <h2 id='how-to-submit-contributions'>
+        Section 3 - How to submit contributions
+      </h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum
+        provident unde fugit distinctio nobis ipsa repudiandae maiores totam
+        numquam. Dolore excepturi atque voluptates velit dolor ad possimus
+        nesciunt quae asperiores?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum
+        provident unde fugit distinctio nobis ipsa repudiandae maiores totam
+        numquam. Dolore excepturi atque voluptates velit dolor ad possimus
+        nesciunt quae asperiores?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum
+        provident unde fugit distinctio nobis ipsa repudiandae maiores totam
+        numquam. Dolore excepturi atque voluptates velit dolor ad possimus
+        nesciunt quae asperiores?
+      </p>
+    </div>
   );
 };
 
