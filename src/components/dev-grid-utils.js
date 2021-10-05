8b6b05e6fb932104fc954fca78605de57d51826c
nlewis84 <nathan@differential.com>
2021-10-04 21:06:43 -0500

    feat: added info for Nathan Lewis

--- src/components/dev-grid-utils.js
+++ src/components/dev-grid-utils.js
@@ -9,16 +9,17 @@ export const profiles = [
   {
     githubUrl: 'https://github.com/MrsWarren',
     githubUsername: 'MrsWarren',
-    linkedinUrl: 'https://www.linkedin.com/in/krystleawarren/', 
+    linkedinUrl: 'https://www.linkedin.com/in/krystleawarren/',
     photo: 'https://avatars.githubusercontent.com/u/31019414?v=4',
     siteUrl: null,
   },
   {
-    githubUrl: 'https://github.com/',
-    githubUsername: null,
-    linkedinUrl: 'https://www.linkedin.com/',
-    photo: null,
-    siteUrl: null,
+    githubUrl: 'https://github.com/nlewis84',
+    githubUsername: 'nlewis84',
+    linkedinUrl: 'https://www.linkedin.com/in/nlewis84',
+    photo:
+      'https://media-exp1.licdn.com/dms/image/C4E03AQF6GRty7hLmdA/profile-displayphoto-shrink_400_400/0/1619664151128?e=1639008000&v=beta&t=E5bVRZmjH3EH2fAKBdA3YQ-FHBYb_WHGdYv5tNM6iRo',
+    siteUrl: 'https://nathanlewis.dev',
   },
   {
     githubUrl: 'https://github.com/',
