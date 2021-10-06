c6b6ccea5092e1f681a78e79b92cb8b702ef87d7
Joe Warren <joe.warren7@protonmail.com>
2021-10-06 08:06:27 -0500

    feat: add google analytics

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,9 +1,15 @@
+import React, { useEffect } from 'react';
+import ReactGA from 'react-ga';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import DevCard from './dev-card';
 import { profiles } from './dev-grid-utils';
 
 const Home = () => {
+  useEffect(() => {
+    ReactGA.initialize(process.env.REACT_APP_TRACKING_ID);
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
   return (
     <>
       <header style={{ textAlign: 'center' }}>
