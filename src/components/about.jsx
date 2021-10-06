64f6fe1e6bd6fb5b415c0fbfd5bf960cfe8c22b7
Krystle Warren <krystleawarren@protonmail.com>
2021-10-05 23:24:27 -0500

    fix: make about page image responsive with mui card

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,13 +1,18 @@
-import Link from '@mui/material/Link';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import GitHubAbout from '../images/github-about.png';
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Link from "@mui/material/Link";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import GitHubAbout from "../images/github-about.png";
 
 const About = () => {
   return (
     <>
-      <Container component='main' sx={{ mt: 8, mb: 2 }} maxWidth='sm'>
-        <Typography variant='h1' component='h1'>
+      <Container component="main" sx={{ mt: 8, mb: 2 }} maxWidth="sm">
+        <Typography variant="h1" component="h1">
           Open Source Devs
         </Typography>
         <Typography>
@@ -26,22 +31,49 @@ const About = () => {
             four required to receive a shirt from Digital Ocean’s Hacktoberfest
             challenge!
           </p>
-          <figure>
-            <img
-              src={GitHubAbout}
-              alt='Open Source Devs GitHub repo about section'
+
+          <Card sx={{ maxWidth: 345, margin: "0 auto" }}>
+            <CardMedia
+              component="img"
+              height="140"
+              image={GitHubAbout}
+              alt="Open Source Devs GitHub repo about section"
             />
-            <figcaption style={{ textAlign: 'center' }}>
-              Hacktoberfest label
-            </figcaption>
-          </figure>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Open Source Devs
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Open Source Devs GitHub repo with hacktoberfest and
+                hacktoberfest-accepted labels
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button
+                href="https://github.com/MightyJoeW/OpenSourceDevs"
+                target="_blank"
+                rel="noopener noreferrer"
+                size="small"
+              >
+                Repo
+              </Button>
+              <Button
+                href="https://www.getscriptordietryin.com/contributing-to-open-source-starter-guide"
+                target="_blank"
+                rel="noopener noreferrer"
+                size="small"
+              >
+                Guide
+              </Button>
+            </CardActions>
+          </Card>
           <p>
             To learn more about the world of open source and how you can get
-            involved, check out my article{' '}
+            involved, check out my article{" "}
             <Link
-              href='https://www.getscriptordietryin.com/Contributing-to-Open-Source-Where-To-Begin-in-2021/'
-              target='_blank'
-              rel='noopener noreferrer'
+              href="https://www.getscriptordietryin.com/Contributing-to-Open-Source-Where-To-Begin-in-2021/"
+              target="_blank"
+              rel="noopener noreferrer"
             >
               Contributing to Open Source: Where to Begin in 2021
             </Link>
@@ -49,11 +81,11 @@ const About = () => {
           </p>
           <p>
             For a step-by-step walkthrough on contributing to the Open Source
-            Devs project, follow the guide{' '}
+            Devs project, follow the guide{" "}
             <Link
-              href='https://www.getscriptordietryin.com/contributing-to-open-source-starter-guide'
-              target='_blank'
-              rel='noopener noreferrer'
+              href="https://www.getscriptordietryin.com/contributing-to-open-source-starter-guide"
+              target="_blank"
+              rel="noopener noreferrer"
             >
               here
             </Link>
@@ -61,11 +93,11 @@ const About = () => {
           </p>
           <p>
             No need for a contribution guide? You can fork and clone the Open
-            Source Devs project{' '}
+            Source Devs project{" "}
             <Link
-              href='https://github.com/MightyJoeW/OpenSourceDevs'
-              target='_blank'
-              rel='noopener noreferrer'
+              href="https://github.com/MightyJoeW/OpenSourceDevs"
+              target="_blank"
+              rel="noopener noreferrer"
             >
               here
             </Link>
@@ -75,9 +107,9 @@ const About = () => {
           <p>Sincerely,</p>
           <p>
             <Link
-              href='https://github.com/MightyJoeW/'
-              target='_blank'
-              rel='noopener noreferrer'
+              href="https://github.com/MightyJoeW/"
+              target="_blank"
+              rel="noopener noreferrer"
             >
               MightyJoeW
             </Link>
