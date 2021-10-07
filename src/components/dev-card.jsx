fef701274f130ae0d35cb427b86aa5f90003c76e
Ryan Riegel <ryan.j.riegel@gmail.com>
2021-10-07 16:40:34 -0400

    added user.jsx and centered the user data using suggested sx attributes. added user route to routes page. added Link inside of dev-card.jsx to redirect to user page when clicking the user image

--- src/components/dev-card.jsx
+++ src/components/dev-card.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Link } from '@reach/router';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -9,14 +10,17 @@ import Typography from '@mui/material/Typography';
 export default function DevCard({ profile }) {
   return (
     <Card sx={{ maxWidth: 345, textAlign: 'center' }}>
-      <CardMedia
-        component='img'
-        image={
-          profile.photo ??
-          'https://avatars.githubusercontent.com/u/9919?s=200&v=4'
-        }
-        alt={`${profile.github} GitHub headshot`}
-      />
+
+      <Link to={`users/${profile.githubUsername}`}>
+        <CardMedia
+          component='img'
+          image={
+            profile.photo ??
+            'https://avatars.githubusercontent.com/u/9919?s=200&v=4'
+          }
+          alt={`${profile.github} GitHub headshot`}
+        />
+      </Link>
 
       <CardContent>
         <Typography gutterBottom variant='h5' component='div'>
