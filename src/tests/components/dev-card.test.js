c71ec277381dbcb4cb14b06192264db78b019bfc
Pavittar Singh <29155477+pavittarx@users.noreply.github.com>
2021-10-08 18:36:07 +0000

    dev cards tests added

--- src/tests/components/dev-card.test.js
+++ src/tests/components/dev-card.test.js
@@ -8,9 +8,45 @@ describe("Dev Card Component", () => {
 
   afterAll(() => cleanup());
 
-  // test("DevCard Renders", () => {
-  //   const { container } = render(<DevCard />);
+  const profile = {
+    githubUrl: 'https://github.com/test-profile-that-does-not-exits',
+    githubUsername: 'test-non-existant-user',
+    linkedinUrl: 'https://www.linkedin.com/in/test-user/',
+    photo: 'https://avatars.githubusercontent.com/u/24866599',
+    siteUrl: 'https://www.test-site.com',
+  };
 
-  //   expect(container.firstChild).toBeDefined();
-  // });
+  test("DevCard Renders", () => {
+    const { container } = render(<DevCard profile={profile} />);
+
+    expect(container.firstChild).toBeDefined();
+  });
+
+  test("Dev Card Renders Name and Photo", () => {
+    const { getByRole, getByText } = render(<DevCard profile={profile} />);
+
+    const nameElement = getByText(profile.githubUsername);
+    expect(nameElement).toBeInTheDocument();
+
+    const photoElement = getByRole("img");
+
+    expect(photoElement).toBeInTheDocument();
+    expect(photoElement).toBeVisible();
+  })
+
+  test("Dev Card Renders All Links", () => {
+    const {getAllByRole} = render(<DevCard profile={profile} />)
+
+    const linkElements = getAllByRole("link");
+
+    expect(linkElements.length).toBe(3);
+
+    linkElements.map(link => {
+      expect(link.href).toBeDefined();
+      expect(link.href).toBeString();
+      expect(link).toBeVisible();
+
+      return null;
+    })
+  })
 })
