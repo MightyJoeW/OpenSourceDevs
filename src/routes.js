f6577e9cc2c5b3a505437c303b5498f15a26a78f
Kapil-Kawathekar <kapilkawathekar8@gmail.com>
2021-10-13 18:10:40 +0530

    removed div

--- src/routes.js
+++ src/routes.js
@@ -5,20 +5,18 @@ import User from "./components/user";
 import Error from "./components/error";
 import { Switch } from "react-router";
 export default (
-  <div>
-    <Switch>
-      <Route path="/" exact>
-        <Home />
-      </Route>
-      <Route path="/about" exact>
-        <About />
-      </Route>
-      <Route path="/users/:username" exact>
-        <User />
-      </Route>
-      <Route path="*" exact>
-        <Error />
-      </Route>
-    </Switch>
-  </div>
+  <Switch>
+    <Route path="/" exact>
+      <Home />
+    </Route>
+    <Route path="/about" exact>
+      <About />
+    </Route>
+    <Route path="/users/:username" exact>
+      <User />
+    </Route>
+    <Route path="*" exact>
+      <Error />
+    </Route>
+  </Switch>
 );
