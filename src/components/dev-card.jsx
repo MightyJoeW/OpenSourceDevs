1a39dce709891ed07d471a2c970a2887b97798c0
11kaze <dustywind11@gmail.com>
2023-03-04 19:35:02 +0530

    Added Some styling to my card (i.e 11kaze)

--- src/components/dev-card.jsx
+++ src/components/dev-card.jsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+// import * as React from "react";
 import { Link, useLocation } from "react-router-dom";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
@@ -8,7 +8,7 @@ import Typography from "@mui/material/Typography";
 import { GitHub, LinkedIn, Language } from "@mui/icons-material";
 import PropTypes from "prop-types";
 import IconButton from "@mui/material/IconButton";
-
+import React, { useEffect } from 'react';
 const disabledLink = {
   pointerEvents: "none",
 };
@@ -19,6 +19,38 @@ const enabledLink = {
 
 export default function DevCard({ profile }) {
   const location = useLocation();
+  // you can add an image/gif with your name here (e.g "11kaze" : "http://image.gif" )
+  const gif_client = {'11kaze':"https://media.tenor.com/t7rc1L-lbIEAAAAC/pokemon.gif"} 
+  useEffect(() => {
+    if (profile['githubUsername'] in gif_client){
+      var aTags = document.getElementsByTagName("div");
+      var searchText = profile['githubUsername'];
+      var textdiv,textdivParent2;
+
+      for (var i = aTags.length-1; i >= 0; i--) {
+        if (aTags[i].textContent === searchText) {
+          textdivParent2 = aTags[i].parentNode.parentNode;
+          textdiv = aTags[i];
+
+          var images = textdiv.getElementsByTagName('img');
+          for(let i=0; i < images.length; i++) {
+            images[i].parentNode.removeChild(images[i]);
+          }
+          var elem = document.createElement("img");
+          elem.setAttribute("src", gif_client[profile['githubUsername']]);
+          elem.setAttribute("width", "30");
+          elem.setAttribute("alt", "buddy");
+          textdiv.appendChild(elem);
+          // textdivParent2.style.boxShadow = "0 0 0.8em 0.4em #a3b5ff3d, 0 0.1em 0.4em #de373757, 0.1em -0.1em 0.1em #4f7eb591";
+          textdivParent2.style.boxShadow = "#f5f5f5 0px 0px 20px inset, #ff00ff2b 20px 0px 30px, #fff0 -20px 0px 20px inset, #ffffff2b 20px 0px 30px inset, #fff -20px 0px 23px inset, #fff 0px 0px 5px, #e69de6b8 -10px 0px 18px, #00ffff70 10px 0px 24px";
+
+          break;
+        }
+      }
+
+    }
+
+  });
 
   return (
     <Card sx={{ maxWidth: 345, textAlign: "center", marginBottom: "4px" }}>
